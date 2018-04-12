450bb42af1080676b7fdd0c670bb5ab0d1da389b
joey <173899378@qq.com>
2018-04-12 17:40:42 +0800

    测试：线程池会吃掉异常，导致没有异常堆栈信息

--- source/joey/thread/threadpool/TestFixPool.java
+++ source/joey/thread/threadpool/TestFixPool.java
@@ -1,11 +1,23 @@
 package joey.thread.threadpool;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by yp-tc-m-7179 on 2018/4/12.
  *
  */
 public class TestFixPool {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("start...");
+                System.out.println(1/0);//线程池会吃掉该异常，导致没有异常堆栈信息
+//                System.out.println(1/1);
+            }
+        });
     }
 }
