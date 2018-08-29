5049b20a2080a9ecb2ac523e25a728b942ef49b6
joey <173899378@qq.com>
2018-08-29 16:01:28 +0800

    加了个闭锁的实例

--- source/joey/thread/sleep/SleepTest.java
+++ source/joey/thread/sleep/SleepTest.java
@@ -1,11 +1,32 @@
 package joey.thread.sleep;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * Created by yp-tc-m-7179 on 2018/2/26.
  *
  */
 public class SleepTest {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception{
+        int num = 50;
+        CountDownLatch start = new CountDownLatch(1);
+        CountDownLatch end = new CountDownLatch(num);
+        for (int i = 0; i < num; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        start.await();
+                        Thread.sleep(3000);
+                        System.out.println(Thread.currentThread().getName());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }finally {
+                        end.countDown();
+                    }
+                }
+            }).start();
+        }
+        start.countDown();
     }
 }
