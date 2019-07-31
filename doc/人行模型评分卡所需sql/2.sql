b58c6dba782ddb6ff3951e170fd14d52423908cb
joey <173899378@qq.com>
2019-07-31 15:33:38 +0800

    相关系数脚本

--- doc/人行模型评分卡所需sql/2.sql
+++ doc/人行模型评分卡所需sql/2.sql
@@ -1,19 +1,20 @@
 create table tmp_2_1 as (
 
-  select B.*,C.year_count as year_count1 from
-    (select distinct Recv_industry from psdas_o_pay_fact )A left join
+  select A.DEBTORNAME,B.Recv_industry,case when C.year_count is null then 0 else c.year_count end  as year_count ,case when D.year_count is null then 0 else D.year_count end as year_count1 from
+    (select distinct DEBTORNAME from psdas_o_pay_fact )A join
+    (select distinct Recv_industry from psdas_o_pay_fact )B left join
     (select DEBTORNAME,sum(Number) as year_count,Recv_industry
      from psdas_o_pay_fact
-     where CONSDATE >= '20170701' and CONSDATE < '20180701' group by DEBTORNAME,Recv_industry)B on A.Recv_industry = B.Recv_industry left join
+     where CONSDATE >= '20170701' and CONSDATE < '20180701' group by DEBTORNAME,Recv_industry)C on B.Recv_industry = C.Recv_industry and A.DEBTORNAME = C.DEBTORNAME left join
     (select DEBTORNAME,sum(Number) as year_count,Recv_industry
      from psdas_o_pay_fact
-     where  CONSDATE >= '20180701' and CONSDATE < '20190701' group by DEBTORNAME,Recv_industry)C on B.Recv_industry  = C.Recv_industry and B.DEBTORNAME = C.DEBTORNAME
+     where  CONSDATE >= '20180701' and CONSDATE < '20190701' group by DEBTORNAME,Recv_industry)D on B.Recv_industry  = D.Recv_industry and A.DEBTORNAME = D.DEBTORNAME
 
 );
 
 create table tmp_2_2 as (
 
-  select DEBTORNAME,sum(year_count)/20 as x_avg,sum(year_count1)/20 as y_avg
+  select DEBTORNAME,sum(year_count)/22 as x_avg,sum(year_count1)/22 as y_avg
   from tmp_2_1 group by DEBTORNAME
 
 );
@@ -24,13 +25,6 @@ create table tmp_2_3 as (
   from tmp_2_1 A inner join tmp_2_2 B on A.DEBTORNAME = B.DEBTORNAME group by A.DEBTORNAME
 
 );
--- select @x_avg := sum(year_count)/20 from tmp_1_1;
-
--- select @y_avg := sum(year_count1)/20 from tmp_1_1;
-
--- select @mo:=sum((year_count-@x_avg)*(year_count1-@y_avg)) from tmp_1_1 group by CRDTORNAME;
-
--- select @de := pow(sum((year_count-@x_avg)*(year_count-@x_avg))*sum((year_count1-@y_avg)*(year_count1-@y_avg)),1/2) from tmp_1_1 group by CRDTORNAME;
 
 create table tmp_2_4_end as (
   select DEBTORNAME,mo/de as pearson from tmp_2_3
