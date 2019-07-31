e6b5e711c8d60858ddd898cb8bbaec6dab15c078
joey <173899378@qq.com>
2019-07-31 09:17:11 +0800

    相关系数脚本

---
+++ doc/人行模型评分卡所需sql/2.sql
@@ -0,0 +1,36 @@
+create table tmp_2_1 as (
+
+  select A.*,B.year_count as year_count1 from
+    (select DEBTORNAME,sum(Number) as year_count,Recv_industry
+     from psdas_o_pay_fact
+     where CONSDATE >= '20170701' and CONSDATE < '20180701' group by DEBTORNAME,Recv_industry)A inner join
+    (select DEBTORNAME,sum(Number) as year_count,Recv_industry
+     from psdas_o_pay_fact
+     where  CONSDATE >= '20180701' and CONSDATE < '20190701' group by DEBTORNAME,Recv_industry)B on A.Recv_industry  = B.Recv_industry and A.DEBTORNAME = B.DEBTORNAME
+
+);
+
+create table tmp_2_2 as (
+
+  select DEBTORNAME,sum(year_count)/20 as x_avg,sum(year_count1)/20 as y_avg
+  from tmp_2_1 group by DEBTORNAME
+
+);
+
+create table tmp_2_3 as (
+
+  select A.DEBTORNAME as DEBTORNAME,sum((A.year_count-B.x_avg)*(A.year_count1-B.y_avg)) as mo,pow(sum((A.year_count-B.x_avg)*(A.year_count-B.x_avg))*sum((A.year_count1-B.y_avg)*(A.year_count1-B.y_avg)),1/2) as de
+  from tmp_2_1 A inner join tmp_2_2 B on A.DEBTORNAME = B.DEBTORNAME group by A.DEBTORNAME
+
+);
+-- select @x_avg := sum(year_count)/20 from tmp_1_1;
+
+-- select @y_avg := sum(year_count1)/20 from tmp_1_1;
+
+-- select @mo:=sum((year_count-@x_avg)*(year_count1-@y_avg)) from tmp_1_1 group by CRDTORNAME;
+
+-- select @de := pow(sum((year_count-@x_avg)*(year_count-@x_avg))*sum((year_count1-@y_avg)*(year_count1-@y_avg)),1/2) from tmp_1_1 group by CRDTORNAME;
+
+create table tmp_2_4_end as (
+  select DEBTORNAME,mo/de as pearson from tmp_2_3
+)
