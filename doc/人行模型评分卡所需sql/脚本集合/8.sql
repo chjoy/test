6517f9f00c6abbeb083d38c270c6696febc610d6
joey <173899378@qq.com>
2019-08-06 12:30:36 +0800

    人行模型脚本

--- doc/人行模型评分卡所需sql/脚本集合/8.sql
+++ doc/人行模型评分卡所需sql/脚本集合/8.sql
@@ -16,6 +16,50 @@ CREATE TABLE rh_score_2 AS (
     n02Score * 1.5 +
     amtRatioScore +
     rateScore * 1.5
-  as score
+      AS score
   FROM rh_score_1
-)
+);
+
+DROP TABLE IF EXISTS rh_score_3;
+CREATE TABLE rh_score_3 AS (
+  SELECT
+    DEBTORNAME,
+    scaleAmtScore * 1.5 +
+    industrycodeScore +
+    areaNameScore +
+    emScore +
+    p01Score +
+    p02Score +
+    h01Score +
+    p03Score +
+    h02SCore +
+    p04Score +
+    n01Score * 1.5 +
+    n02Score * 1.5 +
+    amtRatioScore * 1.5 +
+    rateScore * 1.5
+      AS score
+  FROM rh_score_1
+);
+
+DROP TABLE IF EXISTS rh_score_4;
+CREATE TABLE rh_score_4 AS (
+  SELECT
+    DEBTORNAME,
+    scaleAmtScore * 1.5 +
+    industrycodeScore +
+    areaNameScore +
+    emScore * 1.2 +
+    p01Score +
+    p02Score +
+    h01Score +
+    p03Score * 1.2 +
+    h02SCore * 1.2 +
+    p04Score +
+    n01Score * 1.5 +
+    n02Score * 1.5 +
+    amtRatioScore * 1.2 +
+    rateScore * 1.5
+      AS score
+  FROM rh_score_1
+);
