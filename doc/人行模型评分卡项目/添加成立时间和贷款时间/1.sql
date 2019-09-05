2c65d239cef06fe55925481cf60c12b1bd4b40c4
joey <173899378@qq.com>
2019-09-05 11:31:05 +0800

    人行项目添加好坏标识

--- doc/人行模型评分卡项目/添加成立时间和贷款时间/1.sql
+++ doc/人行模型评分卡项目/添加成立时间和贷款时间/1.sql
@@ -1,5 +1,5 @@
 -- entinfo添加字段
-ALTER TABLE `entinfo` ADD COLUMN `loanDate` date COMMENT '贷款时间' , ADD COLUMN `esDate` date COMMENT '成立日期';
+ALTER TABLE `entinfo` ADD COLUMN `loanDate` date COMMENT '贷款时间' , ADD COLUMN `esDate` date COMMENT '成立日期', ADD COLUMN `ifBad` TINYINT COMMENT '是否坏样本';
 
 
 
