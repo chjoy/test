b2c05212b84a32b3608877717e978ddb3afc60a0
joey <173899378@qq.com>
2019-09-05 16:54:17 +0800

    添加csv导入脚本

---
+++ doc/人行模型评分卡项目/添加成立时间和贷款时间/3.sql
@@ -0,0 +1,8 @@
+load data infile '/Master.csv' into table psdas_o_pay_fact fields terminated by ',';
+
+load data infile '/Master.csv' into table entinfo fields terminated by ',';
+
+show variables like 'secure%';
+
+
+
